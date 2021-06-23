2adb26a529cd67e25a0dc682e92e6217e1cf5edc
Jilu <elizebeth18.jilu@gmail.com>
2021-06-24 03:51:03 +0530

    service file changes

--- src/app/products/product.component.ts
+++ src/app/products/product.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { IProduct } from './product.model';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'app-product',
@@ -13,7 +14,7 @@ import { IProduct } from './product.model';
     ]
 })
 
-export class ProductComponent {
+export class ProductComponent implements OnInit {
     title : string = "Product Filter";
     mainHeading : string = "*****Products Page*****";
     userText : string = "User Input Text";
@@ -22,44 +23,15 @@ export class ProductComponent {
     imageWidth : number = 100;
     serverStatus : string = "Offline";
     products : IProduct []= [
-        {
-           _id: '5a05db08734d1d68d42d3300',
-           productId: 3,
-           productName: 'Hammer',
-           productCode: 'TBX-0048',
-           releaseDate: 'May 21, 2016',
-           description: 'Curved claw steel hammer',
-           price: 8.9,
-           starRating: 1.8,
-           imageUrl: 'https://i.ibb.co/TrR7jkM/hammer.png'
-         },
-         {
-           _id: '5a05db24734d1d68d42d3313',
-           productId: 8,
-           productName: 'Saw',
-           productCode: 'TBX-0022',
-           releaseDate: 'May 15, 2016',
-           description: '15-inch steel blade hand saw',
-           price: 11.55,
-           starRating: 3.7,
-           imageUrl: 'https://i.ibb.co/vmS3kRH/saw.png'
-         },
-         {
-           _id: '5a05db3d734d1d68d42d3322',
-           productId: 10,
-           productName: 'Video Game Controller',
-           productCode: 'GMG-0042',
-           releaseDate: 'October 15, 2015',
-           description: 'Standard two-button video game controller',
-           price: 35.95,
-           starRating: 4.3,
-           imageUrl: 'https://i.ibb.co/HD3RHMW/videogame.jpg'
-         }
    ];
 
-    constructor(){
+   constructor(private productService: ProductService){
       this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
-    }
+   }
+
+   ngOnInit() : void {
+      this.products = this.productService.getProducts();
+   }
 
    toggleImage() :  void {
         this.shoWImage = !this.shoWImage;
